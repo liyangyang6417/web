9e17038e34e72d18139edaa6b57743d745522514
liyangyang6417 <15206544403@163.com>
2018-09-30 10:20:51 +0800

    jQuery练习
    第一次上传

---
+++ jQuery/练习/05轮播图.js
@@ -0,0 +1,51 @@
+/*
+ 功能说明:
+ 1. 点击向右(左)的图标, 平滑切换到下(上)一页
+ 2. 无限循环切换: 第一页的上一页为最后页, 最后一页的下一页是第一页
+ 3. 每隔3s自动滑动到下一页
+ 4. 当鼠标进入图片区域时, 自动切换停止, 当鼠标离开后,又开始自动切换
+ 5. 切换页面时, 下面的圆点也同步更新
+ 6. 点击圆点图标切换到对应的页
+
+ bug: 快速点击下一页时, 有问题
+ */
+$(function(){
+	//1. 点击向右(左)的图标, 平滑切换到下(上)一页
+	var TIME = 400
+	var ITEM_TIME = 20
+	var PAGE_WIDTH = 600
+	var $container = $('#container') 
+	var $list = $('#list')
+	var imgCount = $('#pointsDiv>span').length
+	//向前翻页
+	$('#prev').click(function(){
+		nextPage(false)	
+	})
+	//向后翻页
+	$('#next').click(function(){
+		nextPage(true)
+	})
+	
+	function nextPage(next){
+		var currLeft = 0 
+		var poistion = $list.position().left
+		var targetLeft = poistion+(next?-PAGE_WIDTH:PAGE_WIDTH)
+		var itemOffset = 0
+		var intervalId = setInterval(function(){
+			itemOffset += PAGE_WIDTH/(TIME/ITEM_TIME)
+			currLeft = next?poistion-itemOffset:poistion+itemOffset
+			if(currLeft == targetLeft){
+				if(currLeft == -(imgCount+1)*PAGE_WIDTH){
+					currLeft = -PAGE_WIDTH
+				}else if(currLeft == 0){
+					currLeft = -imgCount*PAGE_WIDTH
+				}
+				clearInterval(intervalId)//取消定时器
+			}
+			
+			$list.css('left',currLeft)
+			
+		},ITEM_TIME)
+	}
+	
+})
